2fae885a075e168ad0dc923921ffabeb8dbb8779
Mohammed <alazzawy.mohammed@gmail.com>
2016-02-11 14:34:07 -0500

    Frogger
    Games have a lot of objects and those objects do a lot of different
    things; but sometimes they do some very similar things as well. This
    creates a great opportunity to practice object-oriented programming, an
    important programming paradigm that influences your application
    architecture and provides performance optimization.

---
+++ README.md
@@ -0,0 +1,20 @@
+frontend-nanodegree-arcade-game version 1.0 12/3/2015
+=====================================================
+
+General Information:
+--------------------
+
+Frogger it's an arcade game (nothing special) use it for killing Time especially when you're bored.
+
+How to install/ play:
+---------------------
+- Make sure you have downloaded all files. Then open index.html in your favorite browser (mine is chrome ^_^)
+
+- Use the arrow keys on your keyboard to move the character.
+
+- sit back and enjoy :)
+
+For more information:
+---------------------
+email: example@gmail.com
+phone: 555-555-5555
---
+++ js/app.js
@@ -0,0 +1,103 @@
+Object.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+// Enemies our player must avoid
+var Enemy = function(x, y) {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
+
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    this.sprite = 'images/enemy-bug.png';
+    this.x = x;
+    this.y = y;
+    this.speed = Math.floor((Math.random() * 200) + 100);
+};
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function(dt) {
+    if (this.x <= 550) {
+        this.x += this.speed * dt;
+    } else {
+        this.x = -2;
+    }
+    if (player.x >= this.x - 30 && player.x <= this.x + 30) {
+        if (player.y >= this.y - 30 && player.y <= this.y + 30) {
+            player.reset();
+        }
+    }
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+};
+
+// Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Now write your own player class
+var Player = function() {
+    this.sprite = 'images/char-boy.png';
+    this.x = 200;
+    this.y = 400;
+};
+Player.prototype.reset = function() {
+    this.x = 200;
+    this.y = 400;
+};
+// This class requires an update(), render() and
+// a handleInput() method.
+Player.prototype.update = function() {
+    //if left key is pressed and player is not on edge of map, pressed decrement x
+    if (this.ctlKey === 'left' && this.x > 0) {
+        this.x = this.x - 50;
+        //if right key is pressed and player is not on edge of map increment x 
+    } else if (this.ctlKey === 'right' && this.x != 400) {
+        this.x = this.x + 50;
+        //if up key is pressed increment y 
+    } else if (this.ctlKey === 'up') {
+        this.y = this.y - 50;
+        //if down key is pressed and player is not on edge of map decrement y 
+    } else if (this.ctlKey === 'down' && this.y != 400) {
+        this.y = this.y + 50;
+    }
+    this.ctlKey = null;
+
+    //If on water, reset
+    if (this.y < 25) {
+        this.reset();
+    }
+};
+
+Player.prototype.handleInput = function(e) {
+    this.ctlKey = e;
+};
+
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+// Place the player object in a variable called player
+var allEnemies = [];
+(function setEnemies() {
+    allEnemies.push(new Enemy(-2, 60));
+    allEnemies.push(new Enemy(-2, 100));
+    allEnemies.push(new Enemy(-2, 150));
+    allEnemies.push(new Enemy(-2, 220));
+}());
+
+var player = new Player();
+
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
